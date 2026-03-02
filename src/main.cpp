94ab7c9b2142038f6e4eeff88192088c0856011d
Priyansh Gupta <priyanshgupta3002@gmail.com>
2026-03-02 12:39:53 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
+#include <filesystem>
+#include <sys/stat.h>
 using namespace std;
+namespace fs = std::filesystem;
 int main()
 {
   // Flush after every std::cout / std:cerr
@@ -9,11 +13,29 @@ int main()
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
+
   while (true)
   {
     cout << "$ ";
     string command;
     getline(std::cin, command);
+    const char *env;
+    const char *env = getenv(env);
+    string pth = env;
+    // std::stringstream ss(pth);
+    vector<string> str;
+    string s = "";
+    for (auto itr : pth)
+    {
+      if (itr == ':')
+      {
+        str.push_back(s);
+        s = "";
+        continue;
+      }
+      s += itr;
+    }
+    str.push_back(s);
     if (command == "exit")
     {
       exit(0);
@@ -22,13 +44,39 @@ int main()
     {
       cout << command.substr(5) << std::endl;
     }
-    else if (command.substr(0, 5) == "type "){
+    else if (command.substr(0, 5) == "type ")
+    {
       string s = command.substr(5);
-      if(s == "echo" || s=="exit" || s=="type"){
+
+      if (s == "echo" || s == "exit" || s == "type")
+      {
         cout << command.substr(5) << " is a shell builtin" << endl;
       }
-      else{
-        cout << command.substr(5) << ": not found" << endl;
+      else
+      {
+        string fn = command.substr(5);
+        int op = 0;
+        for (auto itr : str)
+        {
+          fs::path fullPath = fs::path(itr) / fn;
+          if (fs::exists(fullPath))
+          {
+            struct stat fileStat;
+            if (stat(fullPath.string().c_str(), &fileStat) == 0)
+            {
+              if (fileStat.st_mode && S_IEXEC)
+              {
+                std::cout << "File has execute permission (for owner).\n";
+                op = 1;
+                break;
+              }
+            }
+          }
+        }
+        if (!op)
+        {
+          cout << command.substr(5) << ": not found" << endl;
+        }
       }
     }
     else
